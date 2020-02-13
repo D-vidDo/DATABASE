0d8026a0fbea54291a79213145e07edadae99bca
David Do <55934975+D-vidDo@users.noreply.github.com>
2020-02-12 21:28:05 -0700

    lab5 final push

--- lab5.sql
+++ lab5.sql
@@ -1,23 +1,47 @@
 set echo ON
 set linesize 132
 set pagesize 66
+spool c:/cprg250/lab5output.txt
 
--- QUESTION 2 BUT WITH 32 ROWS INSTEAD OF 21
-SELECT c.firstname, c.lastname, MAX(o.order#) "WOWWWWW", oi.quantity*oi.paideach "Order Ttl"
-FROM customers c
-JOIN orders o ON (c.customer# = o.customer#)
-JOIN orderitems oi ON (o.order# = oi.order#)
-WHERE o.order# = oi.order#
-GROUP BY o.order#, c.firstname, c.lastname, oi.quantity*oi.paideach
-ORDER BY 2,1;
+-- QUESTION 2
+SELECT INITCAP(firstname) "First",
+INITCAP(lastname) "Last",
+order#,
+SUM(paideach*quantity) "Order Ttl"
+FROM orderitems NATURAL JOIN orders NATURAL JOIN customers
+GROUP BY (firstname, lastname, order#)
+ORDER BY 2, 1;
 
--- WIP QUESTION 2
-SELECT customers.firstname, customers.lastname, MAX(order#) "Order#"
-FROM orders JOIN customers ON (orders.customer# = customers.customer#)
-GROUP BY order#, firstname, lastname
-ORDER BY 2,1;
+-- QUESTION 3
+SELECT TO_CHAR(AVG(COUNT(UNIQUE isbn)/COUNT(UNIQUE authorid)), '9.99') "Avg"
+FROM bookauthor
+GROUP BY authorid;
 
--- WIP QUESTION 2 ORDER TOTAL
-SELECT SUM(paideach) "Order Ttl"
-FROM orderitems
-GROUP BY order#;
+-- QUESTION 4
+SELECT INITCAP(category) "Category",
+COUNT(title) "Num of Books", 
+TO_CHAR(AVG(retail), '$99.99') "Average"
+FROM books
+GROUP BY category
+HAVING COUNT(title) >= 2
+ORDER BY 1;
+
+-- QUESTION 5
+SELECT INITCAP(fname) "First",
+INITCAP(lname) "Last",
+SUM(quantity) "# of Books"
+FROM author
+JOIN bookauthor USING (authorid)
+JOIN books USING (isbn)
+JOIN orderitems USING (isbn)
+GROUP BY fname, lname
+HAVING SUM(quantity) >= 10;
+
+-- QUESTION 6 
+SELECT firstname, lastname, SUM(paideach*quantity) "Value"
+FROM orderitems JOIN orders USING (order#)
+JOIN customers USING (customer#)
+GROUP BY GROUPING SETS((firstname, lastname, order#), (firstname, lastname), ())
+ORDER BY 2;
+
+spool off
